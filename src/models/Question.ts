f6d7a0c5f75a5fa3f8cbde22e6406c622785713e
MaorBemdoo <MaorBemdoo@users.noreply.github.com>
2024-10-23 02:13:36 +0000

    style: format files

--- src/models/Question.ts
+++ src/models/Question.ts
@@ -1,26 +1,29 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const questionSchema = new mongoose.Schema({
-    type: {type: String, enum: {
+const questionSchema = new mongoose.Schema(
+  {
+    type: {
+      type: String,
+      enum: {
         values: ["objective", "theory"],
         message: "{VALUE} is not a supported data source",
-    }, required: true},
-    question: {
-        type: String,
-        required: true,
-        trim: true
+      },
+      required: true,
     },
-    options: [
-        {type: String}
-    ],
-    correct_answer: {type: String, required: true},
-    explanation: {type: String},
-    course: {type: String, required: true},
-    tags: [
-        {type: String}
-    ]
-}, {timestamps: true})
+    question: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    options: [{ type: String }],
+    correct_answer: { type: String, required: true },
+    explanation: { type: String },
+    course: { type: String, required: true },
+    tags: [{ type: String }],
+  },
+  { timestamps: true },
+);
 
-const Question = mongoose.model("Question", questionSchema)
+const Question = mongoose.model("Question", questionSchema);
 
-export default Question
+export default Question;
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,55 +1,55 @@
-import NextAuth, { CredentialsSignin } from 'next-auth';
-import bcrypt from 'bcryptjs';
-import Credentials from 'next-auth/providers/credentials';
-import Google from 'next-auth/providers/google';
-import User from '@/models/User';
-import client from '@/config/mongo';
-import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import NextAuth, { CredentialsSignin } from "next-auth";
+import bcrypt from "bcryptjs";
+import Credentials from "next-auth/providers/credentials";
+import Google from "next-auth/providers/google";
+import User from "@/models/User";
+import client from "@/config/mongo";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    adapter: MongoDBAdapter(client),
-    providers: [
-        Google({
-            clientId: process.env.AUTH_GOOGLE_ID,
-            clientSecret: process.env.AUTH_GOOGLE_SECRET,
-        }),
-        Credentials({
-            name: 'Credentials',
-            credentials: {
-                username: { label: "Username", type: "text" },
-                password: { label: "Password", type: "password" }
-            },
-            async authorize(credentials) {
-                class UserNotFoundError extends CredentialsSignin {
-                    message = "User doesn't exist"
-                    status = 404
-                }
+  adapter: MongoDBAdapter(client),
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        class UserNotFoundError extends CredentialsSignin {
+          message = "User doesn't exist";
+          status = 404;
+        }
 
-                class PasswordIncorrectError extends CredentialsSignin {
-                    message = "Invalid Password"
-                    status = 400
-                }
+        class PasswordIncorrectError extends CredentialsSignin {
+          message = "Invalid Password";
+          status = 400;
+        }
 
-                const user = await User.findOne({ username: credentials.username });
+        const user = await User.findOne({ username: credentials.username });
 
-                if(!user){
-                    throw new UserNotFoundError()
-                }
+        if (!user) {
+          throw new UserNotFoundError();
+        }
 
-                if (bcrypt.compareSync(credentials.password as string, user.password)) {
-                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                    const { password, ...userWithoutPassword } = user;
-                    return userWithoutPassword;
-                } else {
-                    throw new PasswordIncorrectError()
-                }
-            }
-        })
-    ],
-    callbacks: {
-        session({ session, user }) {
-            session.user = user
-            return session
-        },
-    }
+        if (bcrypt.compareSync(credentials.password as string, user.password)) {
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          const { password, ...userWithoutPassword } = user;
+          return userWithoutPassword;
+        } else {
+          throw new PasswordIncorrectError();
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    session({ session, user }) {
+      session.user = user;
+      return session;
+    },
+  },
 });
