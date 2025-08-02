3b1dd93c8d3b06b10f595c64eb05c86d92c35c5f
Bemdoo Maor <bemdoo.maor1@gmail.com>
2025-08-02 11:47:48 +0100

    fix

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,33 +1,34 @@
 import pino, { transport } from "pino";
 
 const isEdge = process.env.NEXT_RUNTIME === "edge";
+const isDev = process.env.NODE_ENV === "development"
 
 let logger = pino({ level: "info" });
 
 if (!isEdge) {
-  const devTransport = transport({
-    targets: [
-      {
-        target: "pino-pretty",
-        options: { colorize: true },
-      },
-      {
-        target: "pino/file",
-        options: {
-          destination: "./logs/output.log",
-          mkdir: true,
+  let resolvedTransport;
+  if(isDev){
+    resolvedTransport = transport({
+      targets: [
+        {
+          target: "pino-pretty",
+          options: { colorize: true },
         },
-      },
-    ],
-  });
+        {
+          target: "pino/file",
+          options: {
+            destination: "./logs/output.log",
+            mkdir: true,
+          },
+        },
+      ],
+    });
+  }
 
-  const prodTransport = transport({
+  resolvedTransport = transport({
     target: "@logtail/pino",
     options: { sourceToken: process.env.BETTERSTACK_TOKEN },
   });
-
-  const resolvedTransport =
-    process.env.NODE_ENV === "development" ? devTransport : prodTransport;
 
   logger = pino(resolvedTransport);
 }
