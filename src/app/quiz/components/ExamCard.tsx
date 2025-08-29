270e65796ac580db941c24cf562561e64e764892
Bemdoo Maor <bemdoo.maor1@gmail.com>
2025-08-29 11:39:16 +0100

    style: improve quiz card ui

--- src/app/quiz/components/ExamCard.tsx
+++ src/app/quiz/components/ExamCard.tsx
@@ -5,17 +5,19 @@ import React from 'react'
 
 const ExamCard = ({ title, code, level, semester, session, id }: any) => {
   return (
-    <div className='p-4 divide-y divide-gray-500 rounded-md bg-primary-light-100 shadow-md dark:bg-primary-dark-100 dark:divide-gray-300'>
+    <div className='flex flex-col gap-2 justify-between p-4 rounded-md bg-primary-light-100 shadow-md dark:bg-primary-dark-100'>
       <div className="mb-4">
         <div className='flex justify-between items-center'>
           <p className="text-xs font-mono">{code.split("").join(" ")}</p>
           <p className="text-xs font-mono">E X A M</p>
         </div>
         <p className="font-semibold mt-3 mb-8">{title}</p>
-        <p className='text-sm'>{level} Level · {semester}{semester == 1 ? 'st' : semester == 2 ? 'nd' : semester === 3 ? 'rd' : 'th'} Semester · {session} Session</p>
       </div>
-      <div className='pt-2 text-end'>
-        <AppLink href={`/quiz/${id}`}><Button className='scale-90'>Take</Button></AppLink>
+      <div className='divide-y divide-gray-500 dark:divide-gray-300'>
+        <p className='text-sm'>{level} Level · {semester}{semester == 1 ? 'st' : semester == 2 ? 'nd' : semester === 3 ? 'rd' : 'th'} Semester · {session} Session</p>
+        <div className='text-end pt-2 mt-2'>
+          <AppLink href={`/quiz/${id}`}><Button className='scale-90'>Take</Button></AppLink>
+        </div>
       </div>
     </div>
   )
