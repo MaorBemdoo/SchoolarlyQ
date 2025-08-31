15e6c0daa06fa17eec13826b5804003f94732082
Bemdoo Maor <bemdoo.maor1@gmail.com>
2025-08-31 22:08:04 +0100

    style: update examcard ui

--- src/app/quiz/components/ExamCard.tsx
+++ src/app/quiz/components/ExamCard.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import AppLink from '@/components/AppLink'
-import Button from '@/components/Button'
 import React from 'react'
 
 const ExamCard = ({ title, code, level, semester, session, id }: any) => {
@@ -11,13 +10,10 @@ const ExamCard = ({ title, code, level, semester, session, id }: any) => {
           <p className="text-xs font-mono">{code.split("").join(" ")}</p>
           <p className="text-xs font-mono">E X A M</p>
         </div>
-        <p className="font-semibold mt-3 mb-8">{title}</p>
+        <AppLink href={`/quiz/${id}`} className="font-semibold mt-3 mb-8 hover:underline">{title}</AppLink>
       </div>
-      <div className='divide-y divide-gray-500 dark:divide-gray-300'>
+      <div>
         <p className='text-sm'>{level} Level · {semester}{semester == 1 ? 'st' : semester == 2 ? 'nd' : semester === 3 ? 'rd' : 'th'} Semester · {session} Session</p>
-        <div className='text-end pt-2 mt-2'>
-          <AppLink href={`/quiz/${id}`}><Button className='scale-90'>Take</Button></AppLink>
-        </div>
       </div>
     </div>
   )
