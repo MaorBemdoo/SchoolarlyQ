964a3513458565a654089a4f8d809f3af8fc2fdf
Bemdoo Maor <bemdoo.maor1@gmail.com>
2025-09-10 00:11:16 +0100

    fix: simplify session handling in middleware and remove unused ResponseHandler import

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,7 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 export const runtime = "nodejs";
 
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "./utils/auth";
-import ResponseHandler from "./utils/ResponseHandler";
 
 const protectedRoutes = ["/exams/*", "/admin/*"];
 export const middleware = async (req: NextRequest) => {
@@ -17,13 +15,7 @@ export const middleware = async (req: NextRequest) => {
 
   if (isProtected) {
     const session = await auth();
-
     if (!session) {
-      if (req.nextUrl.pathname.startsWith("/api")) {
-        return NextResponse.json(ResponseHandler("failed", "Unauthorized"), {
-          status: 401,
-        });
-      }
       return NextResponse.redirect(new URL("/auth/login", req.url));
     }
   }
