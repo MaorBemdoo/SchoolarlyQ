68ffd3f989e0971e218c2961a01d457cd572241d
Bemdoo Maor <bemdoo.maor1@gmail.com>
2025-10-04 13:01:44 +0100

    feat: add to veryAnswer

--- src/actions/question.ts
+++ src/actions/question.ts
@@ -6,6 +6,7 @@ import { questionService } from "@/services/question.service";
 import connectDB from "@/utils/db";
 import ResponseHandler from "@/utils/ResponseHandler";
 import { questionSchema, validate } from "@/utils/validators";
+import { sendChatMessage } from "./chat";
 
 interface SingleQuestion {
   question: string;
@@ -87,17 +88,36 @@ export async function verifyAnswer(id: string, mode: string, ans: string) {
 
   try {
     const question = await questionService.getQuestionById(id);
-    if (ans == question.correct_answer) {
-      return ResponseHandler("success", "Answer is correct", {
-        correct_answer: ans,
-        isCorrect: true,
+    if(question.type == "objective") {
+      if (question.correct_answer) {
+        return ResponseHandler("success", "Answer verified successfully", {
+          correct_answer: ans,
+          isCorrect: true,
+        });
+      }
+      return ResponseHandler("success", "Answer verified successfully", {
+        isCorrect: false,
+        ans,
+        correct_answer: question.correct_answer,
       });
     }
-    return ResponseHandler("success", "Answer is incorrect", {
-      isCorrect: false,
-      ans,
-      correct_answer: question.correct_answer,
-    });
+
+    const res = await sendChatMessage(
+      `Given the question: ${question.question}, the user's answer: ${ans} and the correct answer: ${question.correct_answer}, determine if the user's answer is correct(just do an approx scoring). If it is correct, respond with {"isCorrect": true}. If it is incorrect respond with {"isCorrect": false}. Provide your response in valid json format only.`,
+    );
+    console.log(res)
+
+    if (res.status === "success") {
+      const content = res.data;
+      const parsed = JSON.parse(content);
+      return ResponseHandler("success", `Answer verified successfully`, {
+        isCorrect: parsed.isCorrect,
+        ans: !parsed.isCorrect ? ans : undefined,
+        correct_answer: question.correct_answer,
+      });
+    } else {
+      throw new Error(res.message || "Error verifying answer");
+    }
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (err: any) {
     logger.error(err, "Failed to verify answer");
